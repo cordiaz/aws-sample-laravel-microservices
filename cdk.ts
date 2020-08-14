64ee085a4723505af2df3afcec6b811b55971e4f
Petra Barus <petra.barus@gmail.com>
2020-08-15 03:21:01 +0700

    override docker for local.

--- cdk.ts
+++ cdk.ts
@@ -1,5 +1,9 @@
 import * as cdk from '@aws-cdk/core';
 import * as ecs from "@aws-cdk/aws-ecs";
+import { Service as FrontendService } from './frontend/cdk'
+import { Service as Service1 } from './service1/cdk'
+import { Service as Service2 } from './service2/cdk'
+import { Service as Service3 } from './service3/cdk'
 
 class AppStack extends cdk.Stack {
     public readonly cluster: ServicesCluster;
@@ -7,6 +11,19 @@ class AppStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: {}) {
         super(scope, id, props);
         this.cluster = new ServicesCluster(this, 'ServicesCluster');
+        
+        new FrontendService(this, 'Frontend', {
+            cluster: this.cluster.cluster  
+        });
+        new Service1(this, 'Service1', {
+            cluster: this.cluster.cluster  
+        });
+        new Service2(this, 'Service2', {
+            cluster: this.cluster.cluster  
+        });
+        new Service3(this, 'Service3', {
+            cluster: this.cluster.cluster  
+        });
     }
 }
 
